fce44cd4f6fd5c006b99e645c3ec9ae6ac6e38f0
Lina Rodriguez <linam.rodriguez@uniagustiniana.edu.co>
2023-04-02 18:12:45 -0500

    pruebas unitarias

--- app/src/main/java/com/example/agenda/db/DbHelper.java
+++ app/src/main/java/com/example/agenda/db/DbHelper.java
@@ -1,6 +1,8 @@
 package com.example.agenda.db;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -10,7 +12,7 @@ public class DbHelper extends SQLiteOpenHelper {
 
     public static final String Database_Table = "T_usuario";
     public static final String Database_Table1 = "T_producto";
-    private static final int Database_Version = 3;
+    private static final int Database_Version = 4;
     private static final String DATABASE_NAME = "agenda_user.db";
 
     public DbHelper(@Nullable Context context) {
@@ -24,7 +26,7 @@ public class DbHelper extends SQLiteOpenHelper {
                 "contrasena Text)");
 
         sqLiteDatabase.execSQL("CREATE TABLE " + Database_Table1 + "(" +
-                "id Text," +
+                "id Text Primary Key," +
                 "Nombre Text," +
                 "Cantidad Integer)");
 
@@ -38,5 +40,39 @@ public class DbHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE " +
                 Database_Table1);
         onCreate(sqLiteDatabase);
+    }
+
+    public boolean checkUser(String id, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String[] columns = {"id"};
+        String selection = "id=? and contrasena=?";
+        String[] selectionArgs = {id, password};
+        Cursor cursor = db.query(Database_Table, columns, selection, selectionArgs, null, null, null);
+        int count = cursor.getCount();
+        cursor.close();
+        db.close();
+        if (count > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void clearTable() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM " + Database_Table);
+        db.execSQL("INSERT INTO " + Database_Table + " (id, contrasena) VALUES ('admin', 'admin')");
+        db.close();
+    }
+
+    public boolean insertProducto(String id, String nombre, int cantidad) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("id", id);
+        values.put("nombre", nombre);
+        values.put("cantidad", cantidad);
+        db.insert(Database_Table1, null, values);
+        db.close();
+        return true;
     }
 }
--- app/src/test/java/com/example/agenda/ExampleUnitTest.java
+++ app/src/test/java/com/example/agenda/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.agenda;
+package com.example.basedatos;
 
 import org.junit.Test;
 
